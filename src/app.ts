9cf996593593bc2662ff2007077df478fa3f3454
Enrique Vierne <110237705+enriquevierne@users.noreply.github.com>
2023-10-24 17:49:52 -0300

    feat: add config CORS

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import "reflect-metadata";
+import cors from "cors";
 import "express-async-errors";
 import express, { Application } from "express";
 import middlewares from "./middlewares";
@@ -6,11 +7,17 @@ import { anouncementRoute, sessionRoute, userRoute } from "./routes";
 
 const app: Application = express();
 app.use(express.json());
-
-app.use("/api/login", sessionRoute)
-app.use("/api/users", userRoute)
-app.use("/api/anouncements", anouncementRoute)
+app.use(cors());
+const optionsCors = {
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
+app.use("/api/login", sessionRoute);
+app.use("/api/users", userRoute);
+app.use("/api/anouncements", anouncementRoute);
 
 app.use("/api", middlewares.handleError);
+
+app.use("", cors(optionsCors), middlewares.handleError);
 
 export default app;
